3d590e0d7855744bfb1e8f1961ca08172d1c27f0
Minh <minhnguyen47431@gmail.com>
2024-08-27 16:21:50 +0700

    Initial Commit

---
+++ weatherly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weatherly
+//
+//  Created by Nguyễn Minh on 27/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weatherly/weatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherlyApp.swift
+//  weatherly
+//
+//  Created by Nguyễn Minh on 27/8/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
